caa3747e46fd986fa1435890384748f0fdb613fd
nikita zinich <nikitazinich4@gmail.com>
2024-10-08 12:46:54 +0600

    success popup

--- Js/mask-phone.js
+++ Js/mask-phone.js
@@ -1,6 +1,7 @@
-const PHONE__MASK = document.getElementById("tel");
-const PHONE__MASK1 = document.getElementById("tel1");
-const PHONE__MASK2 = document.getElementById("tel3");
-IMask(PHONE__MASK, { mask: "+{7} (000) 000-00-00" });
-IMask(PHONE__MASK1, { mask: "+{7} (000) 000-00-00" });
-IMask(PHONE__MASK2, { mask: "+{7} (000) 000-00-00" });
+const telInputs = document.querySelectorAll("#tel");
+
+telInputs.forEach((input) => {
+  IMask(input, {
+    mask: "+{7} (000) 000-00-00",
+  });
+});
--- Js/modal-first.js
+++ Js/modal-first.js
@@ -1,21 +1,91 @@
-const BUTTON__CLOSE = document.querySelector(".call__close");
-const BUTTON__OPEN = document.querySelector(".nav__btn");
-const BLOK = document.querySelector(".call");
-const BACKGROUND__MODAL__WINDOW = document.querySelector(
-  ".background__modal-window"
-);
+const popupLinks = document.querySelectorAll(".popup-links");
+const body = document.body;
+const lockPadding = document.querySelectorAll(".lock-padding");
 
-function openWindow() {
-  BLOK.style.visibility = "visible";
-  BACKGROUND__MODAL__WINDOW.style.visibility = "visible";
-  document.body.classList.add("modal-open");
+let unlock = true;
+
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupLinks.length; index++) {
+    const popupLink = popupLinks[index];
+    popupLink.addEventListener("click", function (e) {
+      const popupName = popupLink.getAttribute("href").replace("#", "");
+      const currentPopup = document.getElementById(popupName);
+      popupOpen(currentPopup);
+      e.preventDefault();
+    });
+  }
+}
+const popupCloseIcon = document.querySelectorAll(".close__popup");
+if (popupLinks.length > 0) {
+  for (let index = 0; index < popupCloseIcon.length; index++) {
+    const el = popupCloseIcon[index];
+    el.addEventListener("click", function (e) {
+      popupClose(el.closest(".popup"));
+      e.preventDefault();
+    });
+  }
 }
 
-function closeWindow() {
-  BLOK.style.visibility = "hidden";
-  BACKGROUND__MODAL__WINDOW.style.visibility = "hidden";
-  document.body.classList.remove("modal-open");
+function popupOpen(currentPopup) {
+  if (currentPopup && unlock) {
+    const popupActive = document.querySelector(".popup.open");
+    if (popupActive) {
+      popupClose(popupActive, false);
+    } else {
+      bodyLock();
+    }
+    currentPopup.classList.add("open");
+    currentPopup.addEventListener("click", function (e) {
+      // Проверяем, если клик был не внутри .popup__content
+      if (!e.target.closest(".popup__content")) {
+        popupClose(currentPopup);
+      }
+    });
+  }
 }
-BUTTON__OPEN.addEventListener("click", openWindow);
-BUTTON__CLOSE.addEventListener("click", closeWindow);
 
+function bodyLock() {
+  const LockPaddingValue =
+    window.innerWidth - document.querySelector(".wrapper").offsetWidth + "px";
+  for (let index = 0; index < lockPadding.length; index++) {
+    const el = lockPadding[index];
+    el.computedStyleMap.paddingRight = LockPaddingValue;
+  }
+  body.style.paddingRight = LockPaddingValue;
+  body.classList.add("lock");
+  // unlock = false;
+}
+
+function popupClose(popupActive, doUnlock = true) {
+  console.log(unlock);
+  if (unlock) {
+    popupActive.classList.remove("open");
+    if (doUnlock) {
+      bodyUnLock();
+    }
+  }
+}
+
+function bodyUnLock() {
+  setTimeout(function () {
+    if (lockPadding.length > 0) {
+      for (let index = 0; index < lockPadding.length; index++) {
+        const el = lockPadding[index];
+        el.style.paddingRight = "0px";
+      }
+    }
+    body.style.paddingRight = "0px";
+    body.classList.remove("lock");
+  }, 0);
+  unlock = false;
+  setTimeout(function () {
+    unlock = true;
+  }, 1);
+}
+
+document.addEventListener("keydown", function (e) {
+  if (e.which === 27) {
+    const popupActive = document.querySelector(".popup.open");
+    popupClose(popupActive);
+  }
+});
